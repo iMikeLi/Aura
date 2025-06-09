72666349f38204e3e944713564cccdb3288b22cb
iMikeLi <mlinz20@yahoo.com>
2025-06-09 09:30:58 +0200

    156

--- Source/Aura/Public/AuraPlayerController.h
+++ Source/Aura/Public/AuraPlayerController.h
@@ -30,7 +30,7 @@ public:
 
 
 	UFUNCTION(Client, Reliable)
-	void ShowDamageNumber(float DamageAmount, ACharacter* TargetCharacter);
+	void ShowDamageNumber(float DamageAmount, ACharacter* TargetCharacter, bool bBlockedHit, bool bCriticalHit);
 
 protected:
 	virtual void BeginPlay() override;

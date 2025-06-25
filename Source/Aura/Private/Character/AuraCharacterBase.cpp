5d0b4d53bb99cbfe68cdfa814baff95aee60179c
iMikeLi <mlinz20@yahoo.com>
2025-06-25 09:57:01 +0200

    181

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -52,6 +52,7 @@ void AAuraCharacterBase::MulticastHandleDeath_Implementation()
 
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	Dissolve();
+	bDead = true;
 }
 
 void AAuraCharacterBase::BeginPlay()
@@ -65,6 +66,17 @@ FVector AAuraCharacterBase::GetCombatSocketLocation_Implementation()
 	check(Weapon);
 	return Weapon->GetSocketLocation(WeapoonTipSocketName);
 }
+
+bool AAuraCharacterBase::IsDead_Implementation() const
+{
+	return bDead;
+}
+
+AActor* AAuraCharacterBase::GetAvatar_Implementation()
+{
+	return this;
+}
+
 
 void AAuraCharacterBase::InitAbilityActorInfo()
 {

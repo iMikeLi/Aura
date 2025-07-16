3d32a6aff104fce70d109947f56ed4d72a864d23
iMikeLi <mlinz20@yahoo.com>
2025-07-16 06:06:31 +0200

    285
    Added a multicast delegate and client RPC to notify clients when an ability's status changes. Updated ability status is now broadcast to the UI via SpellMenuWidgetController, ensuring the UI reflects the current state after level changes or ability updates.

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -3,6 +3,10 @@
 
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/Data/AbilityInfo.h"
+
+
 void USpellMenuWidgetController::BroadcastInitialValues()
 {
 	BroadcastAbilityInfo();
@@ -10,5 +14,13 @@ void USpellMenuWidgetController::BroadcastInitialValues()
 
 void USpellMenuWidgetController::BindCallbacksToDependencies()
 {
-
+	GetAuraASC()->AbilityStatusChanged.AddLambda([this](const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag)
+		{
+			if (AbilityInfo)
+			{
+				FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoForTag(AbilityTag);
+				Info.StatusTag = StatusTag;
+				AbilityInfoDelegate.Broadcast(Info);
+			}
+		});
 }
